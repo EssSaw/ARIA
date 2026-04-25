49836cb0114b099f55aa4966ea8bd746c829ab41
EssSaw <dungpham1242@gmail.com>
2026-04-25 19:42:33 +0700

    init: scaffolded BE project structure

---
+++ src/test/java/esssaw/aria/AriaApplicationTests.java
@@ -0,0 +1,13 @@
+package esssaw.aria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
